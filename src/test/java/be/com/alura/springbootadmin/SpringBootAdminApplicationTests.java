18032e72bf9c44c176d1d5f4d67eaad1127e1119
Caio Souza <caio.7s@outlook.com>
2020-01-21 09:32:21 -0300

    Monitoramento Spring Boot Admin.

---
+++ src/test/java/be/com/alura/springbootadmin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,12 @@
+package be.com.alura.springbootadmin;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
